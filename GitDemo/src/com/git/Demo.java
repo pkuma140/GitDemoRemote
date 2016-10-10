432c541d1552997ff6c2ab07fc7a591e35d900f8
Pramod Kumar <pramod.j.kumar@capgemini.com>
2016-10-10 17:39:21 +0530

    Initial check-in

---
+++ GitDemo/src/com/git/Demo.java
@@ -0,0 +1,19 @@
+package com.git;
+
+/**
+ * TODO pkuma140 This type ...
+ *
+ * @author pkuma140
+ */
+public class Demo {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    System.out.println("This is my first demo project");
+
+  }
+
+}
